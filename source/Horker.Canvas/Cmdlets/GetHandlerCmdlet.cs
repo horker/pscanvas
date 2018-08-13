a5881ffa7949b1ea0785088e2f1fe4de2de8d47f
horker <horker@users.noreply.github.com>
2018-08-14 02:37:17 +0900

    Implement Get-CanvasHandler

---
+++ source/Horker.Canvas/Cmdlets/GetHandlerCmdlet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Management.Automation;
+using System.Windows;
+
+#pragma warning disable CS1591
+
+namespace Horker.Canvas
+{
+    public class HandlerInfo
+    {
+        public Type Type;
+        public string FileExtension;
+        public IHandler Handler;
+    };
+
+    [Cmdlet("Get", "CanvasHandler")]
+    public class GetCanvasHandler : PSCmdlet
+    {
+        protected override void EndProcessing()
+        {
+            foreach (var entry in HandlerSelector.Instance.TypeHandlerMap)
+            {
+                var info = new HandlerInfo()
+                {
+                    Type = entry.Key,
+                    Handler = entry.Value
+                };
+
+                WriteObject(info);
+            }
+
+            foreach (var entry in HandlerSelector.Instance.FileExtensionHandlerMap)
+            {
+                var info = new HandlerInfo()
+                {
+                    FileExtension = entry.Key,
+                    Handler = entry.Value
+                };
+
+                WriteObject(info);
+            }
+        }
+    }
+}
